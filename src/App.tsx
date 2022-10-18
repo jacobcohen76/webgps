f5e4f71e8fd0109d85a42a789e5af15adb3cc0e0
Jacob Cohen <jacobcohen76@protonmail.com>
2022-10-18 11:22:40 -0700

    added code

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import GpsTracker from './GpsTracker'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <GpsTracker />
+    </div>
+  )
+}
---
+++ src/GpsTracker.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+export default function GpsTracker() {
+  const [latitude, setLatitude] = useState<number>()
+  const [longitude, setLongitude] = useState<number>()
+  useEffect(() => {
+    const interval = setInterval(() => {
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        console.log(coords)
+        setLatitude(coords.latitude)
+        setLongitude(coords.longitude)
+      })
+    }, 100)
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div>
+      <div>Latitude: {latitude}</div>
+      <div>Longitude: {longitude}</div>
+    </div>
+  )
+}
